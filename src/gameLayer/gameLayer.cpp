451446343e5d93523a0dba09d4b542d6c44d7212
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-06-28 12:35:51 +0200

    Save

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -1,14 +1,31 @@
 #include "gameLayer.hpp"
 
+struct GameData
+{
+	// ADD YOUR GAME DATA HERE
+};
+
+
 // Function to initialize the game
-void initGame() {}
+void initGame()
+{
+	// INITIALIZE YOUR GAME DATA HERE
+}
+
 
 // Function to update the game state
-void updateGame(float deltaTime) {
+void updateGame(float deltaTime)
+{
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
+
+	// ADD YOUR GAME LOGIC HERE
 }
+
 
 // Function which runs after the game is closed
 // use it to save your data, free memory, etc.
-void closeGame() {}
+void closeGame()
+{
+	// FREE YOUR GAME DATA HERE
+}
