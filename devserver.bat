a2066c72277c367f2b20a20df251025e0063ef7d
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-06-29 21:39:02 +0200

    savW

--- devserver.bat
+++ devserver.bat
@@ -1,14 +1,7 @@
 @echo off
 
-echo Starting Dev Server via NPM
-echo the web build can now be played in the browser
-echo 
-echo INFO The webbuild has to be build before via web_build.bat
+echo INFO: "Run npm i" first!
 
-start "" "http://localhost:8080/mygame.html"
-echo
-
-npm i
-npx http-server build
+npx seg dev
 
 pause
