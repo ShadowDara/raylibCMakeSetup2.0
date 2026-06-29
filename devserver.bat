e44b197656fe1baa9c38c69e90c600ef9898321e
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-06-29 11:45:39 +0200

    Aktualisieren von devserver.bat

--- devserver.bat
+++ devserver.bat
@@ -3,7 +3,7 @@
 echo Starting Dev Server via NPM
 echo the web build can now be played in the browser
 echo 
-echo INFO: The webbuild has to be build before via web_build.bat
+echo INFO The webbuild has to be build before via web_build.bat
 
 start "" "http://localhost:8080/mygame.html"
 echo
