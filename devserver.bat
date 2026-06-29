a09bf11361e5c1dde868e6ced0968e94634bb907
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-06-29 11:04:26 +0200

    save

---
+++ devserver.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo Starting Dev Server via NPM
+echo the web build can now be played in the browser
+echo 
+echo INFO: The webbuild has to be build before via web_build.bat
+
+start "" "http://localhost:8080/mygame.html"
+echo
+
+npm i
+npx http-server build
+
+pause
