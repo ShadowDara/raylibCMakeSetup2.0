97ac97a5942f8f85a069a61ed16cfc0aa8011cef
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-06-30 22:51:25 +0200

    sav

--- include/platform/utility.hpp
+++ include/platform/utility.hpp
@@ -1,1 +1,25 @@
+// by Shadowdara
+// https://shadowdara.github.io/wp
+
 #pragma once
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+
+// Parser for a simple 2D map format
+// feel free to expand it to your needs
+
+
+struct Map2D {
+	int width;
+	int height;
+	std::vector<int> data;
+};
+
+
+Map2D importMap(std::string input);
+
+
+std::string exportMap(Map2D map);
--- src/platform/utility.cpp
+++ src/platform/utility.cpp
@@ -1,1 +1,33 @@
+// by Shadowdara
+// https://shadowdara.github.io/wp
+
 #include "utility.hpp"
+
+
+Map2D importMap(std::string input)
+{
+	Map2D map;
+	std::istringstream iss(input);
+	iss >> map.width >> map.height;
+	map.data.resize(map.width * map.height);
+	for (int y = 0; y < map.height; ++y) {
+		for (int x = 0; x < map.width; ++x) {
+			iss >> map.data[y * map.width + x];
+		}
+	}
+	return map;
+}
+
+
+std::string exportMap(Map2D map)
+{
+	std::string output;
+	output += std::to_string(map.width) + " " + std::to_string(map.height) + "\n";
+	for (int y = 0; y < map.height; ++y) {
+		for (int x = 0; x < map.width; ++x) {
+			output += std::to_string(map.data[y * map.width + x]) + " ";
+		}
+		output += "\n";
+	}
+	return output;
+}
